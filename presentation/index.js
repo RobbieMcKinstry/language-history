31cf4805c686edee3239c05be96a857d9c510c5c
Robbie McKinstry <robbie@hashicorp.com>
2018-02-19 14:54:55 -0500

    more slides

--- presentation/index.js
+++ presentation/index.js
@@ -12,7 +12,6 @@ import {
   Quote,
   Slide,
   Text,
-  hasSlideChildren
 } from "spectacle";
 
 // Import theme
@@ -37,21 +36,15 @@ export default class Presentation extends React.Component {
       <Deck transition={["zoom", "slide"]} 
             transitionDuration={500} 
             theme={theme}>
+
+        {/* Title */}
         <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             The History of Digital <br/>Programming Languages
           </Heading>
         </Slide>
-      </Deck>
-    );
-  }
-}
-        /*
-        // Title
 
-
-
-        // About me
+        {/* About me */}
         <Slide transition={["fade"]} bgColor="tertiary">
             <Text size={2} textColor="secondary">
                 Robbie McKinstry
@@ -67,18 +60,186 @@ export default class Presentation extends React.Component {
             </Text>
         </Slide>
 
-        // Overview
-        //<Slide transition={["fade"]} bgColor="tertiary">
-          // <Heading size={2} textColor="secondary" caps>Overview</Heading>
-          //<List>
-          //  <ListItem>Early Foundational Langs</ListItem>
-          //  <ListItem>Structured Languages</ListItem>
-          //  <ListItem>1980s Experimentation</ListItem>
-          //</List>
-        // </Slide>
+        {/* Overview */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={2} textColor="secondary" caps>Overview</Heading>
+          <List>
+            <ListItem>Early Foundational Langs</ListItem>
+            <ListItem>Structured Languages</ListItem>
+            <ListItem>1980s Experimentation</ListItem>
+          </List>
+        </Slide>
+
+        {/* The Lambda Calculus */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={2} textColor="secondary" caps>The Lambda Calculus</Heading>
+          <Text size={3} textColor="secondary">
+            A mathematical system for expressing functions
+          </Text>
+          <Text size={3} textColor="secondary">
+            variable bindings, abstractions, and eventually types
+          </Text>
+        </Slide>
+
+        {/* The Lambda Calculus 2 */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Text size={3} textColor="secondary">
+            Modern subset published in 1936 by Alonzo Church
+          </Text>
+          <Text size={3} textColor="secondary">
+            Same year Alan Turing defined the Turing Machine
+          </Text>
+        </Slide>
+
+        {/* Church-Turing Thesis */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={2} textColor="secondary" caps>Church-Turing Thesis</Heading>
+          <Text size={3} textColor="secondary">
+            Showed the Lambda Calculus == Turing Machines
+          </Text>
+          <Text size={3} textColor="secondary">
+            Showed computers could express any mathematical algorithm
+          </Text>
+        </Slide>
+
+        {/* No computer */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Text size={3} textColor="secondary">
+            No computer with programmable digital memory would exist for 20 years until Turing invented one to defeat the Nazis
+          </Text>
+        </Slide>
+        
+        {/* Language but no computer */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Text size={3} textColor="secondary">
+            We had an impractical language, but no computer powerful enough to express it
+          </Text> 
+        </Slide>
 
 
+        {/* Fortran Challenges */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={2} textColor="secondary">Fortran</Heading>
+          <Text size={3} textColor="secondary">
+            Computing time was expensive
+          </Text>
+          <Text size={3} textColor="secondary">
+            Programmer labor was cheap
+          </Text>
+          <Text size={3} textColor="secondary">
+            Wide skepticism that computers could write efficient machine code
+          </Text>
+        </Slide>
 
+
+        {/* Fortran 2 */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Text size={3} textColor="secondary">
+            Released in 1956 at IBM by John Backus
+          </Text>
+          <Text size={3} textColor="secondary">
+            First efficient compiler
+          </Text>
+          <Text size={3} textColor="secondary">
+            Developed to allow scientists and mathematicians to code using a language similar to equation syntax instead of having to learn machine code.
+          </Text>
+          <Text>
+            First time programmers had a "for" loop
+          </Text>
+        </Slide>
+
+
+        {/* Dijkstra Quote 1 */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <BlockQuote>
+            <Quote textSize={72}>The sooner we can forget that Fortran ever existed the better.
+            </Quote>
+            <Cite>
+            Edsger W. Dijkstra, ACM 1972 Turing Award Speech
+            </Cite>
+          </BlockQuote>
+        </Slide>
+
+
+        {/* Dijkstra Quote 2 */}
+         <Slide transition={["fade"]} bgColor="tertiary">
+          <BlockQuote>
+            <Quote textSize={36}>
+                Fortran&#39;s tragic fate has been its wide acceptance, mentally chaining thousands and thousands of programmers to our past mistakes. 
+            </Quote>
+            <Cite>
+            Edsger W. Dijkstra, ACM 1972 Turing Award Speech
+            </Cite>
+          </BlockQuote>
+        </Slide>
+
+        {/* Dijkstra Quote 3 */}
+         <Slide transition={["fade"]} bgColor="tertiary">
+          <BlockQuote>
+            <Quote textSize={36}>
+                I pray daily that that more of my fellow-programmers may find the means of freeing themselves from the curse of compatibility.
+            </Quote>
+            <Cite>
+            Edsger W. Dijkstra, ACM 1972 Turing Award Speech
+            </Cite>
+          </BlockQuote>
+        </Slide>
+
+        {/* Lisp */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={2} textColor="secondary">Lisp</Heading>
+          <Text size={3} textColor="secondary">
+            Invented by John McCarthy (father of modern AI) in 1957
+          </Text>
+ 
+          <Text size={3} textColor="secondary">
+            designed as an abstraction over the lambda calculus
+          </Text>
+          <Text size={3} textColor="secondary">
+            not implemented for years, though eventually given successful hardware support via "Lisp Machines"
+          </Text>
+        </Slide>
+
+        {/* Lisp Features */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Text size={3} textColor="secondary">
+            Heavily used Linked List, Recursion
+          </Text>
+ 
+          <Text size={3} textColor="secondary">
+            Metaprogramming made easy with macros
+          </Text>
+          <Text size={3} textColor="secondary">
+            Highly consistent: everything is either a function or a linked-list node
+          </Text>
+        </Slide>
+
+        {/* Lisp Features */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Text size={3} textColor="secondary">
+            Heavily used Linked List, Recursion
+          </Text>
+ 
+          <Text size={3} textColor="secondary">
+            Metaprogramming made easy with macros
+          </Text>
+        </Slide>
+
+        {/* ML vs C */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <BlockQuote>
+            <Quote textSize={72}>C and ML were both finished in 73. ML had first-class functions, GC, type inference, algebraic data types, pattern matching, and exceptions.
+            </Quote>
+            <Cite>
+                Gary Bernhardt (garybernhardt) May 20, 2015
+            </Cite>
+          </BlockQuote>
+        </Slide>
+      </Deck>
+    );
+  }
+}
+        /*
         <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>Typography</Heading>
           <Heading size={1} textColor="secondary">Heading 1</Heading>
@@ -101,7 +262,7 @@ export default class Presentation extends React.Component {
           </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
+
             <Quote>Example Quote</Quote>
             <Cite>Author</Cite>
           </BlockQuote>
